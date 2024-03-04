d56c718fde153ce70ad732668687a4c35baaa0d0
Thalita <Thalita@THALITA-PC>
2024-03-04 20:52:59 -0300

    Adicionar arquivos de projeto.

---
+++ TP01App/Pessoa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP01App
+{
+    internal class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public string CPF { get; set; }
+    }
+}
---
+++ TP01App/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace TP01App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa = new Pessoa();
+            pessoa.Nome = "Exemplo";
+            pessoa.Idade = 25;
+            pessoa.CPF = "123.456.789-00";
+
+            Console.WriteLine($"Nome: {pessoa.Nome}, Idade: {pessoa.Idade}, CPF: {pessoa.CPF}");
+
+            List<Pessoa> pessoas = new List<Pessoa>();
+
+            while (true)
+            {
+                Console.WriteLine("1 - Adicionar Pessoa | 2 - Mostrar Pessoas");
+                int escolha = Convert.ToInt32(Console.ReadLine());
+
+                if (escolha == 1)
+                {
+                    Pessoa novaPessoa = new Pessoa();
+                    Console.WriteLine("Digite o nome:");
+                    novaPessoa.Nome = Console.ReadLine();
+                    Console.WriteLine("Digite a idade:");
+                    novaPessoa.Idade = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Digite o CPF:");
+                    novaPessoa.CPF = Console.ReadLine();
+
+                    pessoas.Add(novaPessoa);
+                }
+                else if (escolha == 2)
+                {
+                    Console.WriteLine("Pessoas na lista:");
+                    foreach (var p in pessoas)
+                    {
+                        Console.WriteLine($"Nome: {p.Nome}, Idade: {p.Idade}, CPF: {p.CPF}");
+                    }
+                }
+            }
+        }
+    }
+}
